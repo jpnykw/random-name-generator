7bc3b8c047f948bf1e01b7ab6bc2b8b9beca6e9f
Haruto Hirakawa <31243896+jpnykw@users.noreply.github.com>
2021-06-05 00:07:50 +0900

    母音の生成確率

--- generator.js
+++ generator.js
@@ -1,16 +1,24 @@
 const vowel = [65, 73, 85, 69, 79];
 
-const generate = (len) => {
+const generate = (len, vowel_prob) => {
     return Array(len).fill(null).map((_, i) => {
         const chr_code = 65 + (Math.random() * 26) >> 0;
         if ((i + 1) % 2 === 0) {
-            if (vowel.includes(chr_code)) {
-                return String.fromCharCode(chr_code);
+            const is_vowel = Math.random() <= vowel_prob;
+            if (is_vowel) {
+                if (vowel.includes(chr_code)) {
+                    return String.fromCharCode(chr_code);
+                } else {
+                    return String.fromCharCode(vowel[(Math.random() * vowel.length) >> 0]);
+                }
             } else {
-                return String.fromCharCode(vowel[(Math.random() * vowel.length) >> 0]);
+                new_chr_code = 65 + (Math.random() * 26) >> 0;
+                while (vowel.includes(new_chr_code)) new_chr_code = 65 + (Math.random() * 26) >> 0;
+                return String.fromCharCode(new_chr_code);
             }
         } else {
             return String.fromCharCode(chr_code);
         }
+
     }).join('');
 }
